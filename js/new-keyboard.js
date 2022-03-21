b0c694f6cada418dfdfbb62fdfe7b3641bd27eef
Farook Mohammad <97977293+farook8090@users.noreply.github.com>
2022-03-21 13:57:13 +0530

    Add files via upload

---
+++ js/new-keyboard.js
@@ -0,0 +1,283 @@
+
+// & onclick
+
+function changealphabet(small_alphabet) {
+    // This function will make the small alphabet get print on big alphabet.
+    document.getElementById("big-image").src = "img-alphabet/" + small_alphabet;
+    // alert('error');
+}
+
+// & adding Sounds
+function sounds(key_sound, small_alphabet) {
+
+    var audio = document.getElementById('audio');
+
+    switch (key_sound) {
+
+        // Number Row
+        case 'one':
+            document.getElementById("audiosrc").src = "sounds/one.mp3";
+            audio.load(); //call this to just preload the audio without playing.
+            audio.play(); //call this to play the song right away.
+            document.getElementById("big-image").src = "img-alphabet/" + small_alphabet;
+            break;
+        case 'two':
+            document.getElementById("audiosrc").src = "sounds/two.mp3";
+            audio.load(); //call this to just preload the audio without playing.
+            audio.play(); //call this to play the song right away.
+            document.getElementById("big-image").src = "img-alphabet/" + small_alphabet;
+            break;
+        case 'three':
+            document.getElementById("audiosrc").src = "sounds/three.mp3";
+            audio.load(); //call this to just preload the audio without playing.
+            audio.play(); //call this to play the song right away.
+            document.getElementById("big-image").src = "img-alphabet/" + small_alphabet;
+            break;
+        case 'four':
+            document.getElementById("audiosrc").src = "sounds/four.mp3";
+            audio.load(); //call this to just preload the audio without playing.
+            audio.play(); //call this to play the song right away.
+            document.getElementById("big-image").src = "img-alphabet/" + small_alphabet;
+            break;
+        case 'five':
+            document.getElementById("audiosrc").src = "sounds/five.mp3";
+            audio.load(); //call this to just preload the audio without playing.
+            audio.play(); //call this to play the song right away.
+            document.getElementById("big-image").src = "img-alphabet/" + small_alphabet;
+            break;
+        case 'six':
+            document.getElementById("audiosrc").src = "sounds/six.mp3";
+            audio.load(); //call this to just preload the audio without playing.
+            audio.play(); //call this to play the song right away.
+            document.getElementById("big-image").src = "img-alphabet/" + small_alphabet;
+            break;
+        case 'seven':
+            document.getElementById("audiosrc").src = "sounds/seven.mp3";
+            audio.load(); //call this to just preload the audio without playing.
+            audio.play(); //call this to play the song right away.
+            document.getElementById("big-image").src = "img-alphabet/" + small_alphabet;
+            break;
+        case 'eight':
+            document.getElementById("audiosrc").src = "sounds/eight.mp3";
+            audio.load(); //call this to just preload the audio without playing.
+            audio.play(); //call this to play the song right away.
+            document.getElementById("big-image").src = "img-alphabet/" + small_alphabet;
+            break;
+        case 'nine':
+            document.getElementById("audiosrc").src = "sounds/nine.mp3";
+            audio.load(); //call this to just preload the audio without playing.
+            audio.play(); //call this to play the song right away.
+            document.getElementById("big-image").src = "img-alphabet/" + small_alphabet;
+            break;
+        case 'zero':
+            document.getElementById("audiosrc").src = "sounds/zero.mp3";
+            audio.load(); //call this to just preload the audio without playing.
+            audio.play(); //call this to play the song right away.
+            document.getElementById("big-image").src = "img-alphabet/" + small_alphabet;
+            break;
+        // Row 1st
+        case 'q':
+            document.getElementById("audiosrc").src = "sounds/q.mp3";
+            audio.load(); //call this to just preload the audio without playing.
+            audio.play(); //call this to play the song right away.
+            document.getElementById("big-image").src = "img-alphabet/" + small_alphabet;
+            break;
+        case 'w':
+            document.getElementById("audiosrc").src = "sounds/w.mp3";
+            audio.load(); //call this to just preload the audio without playing.
+            audio.play(); //call this to play the song right away.
+            document.getElementById("big-image").src = "img-alphabet/" + small_alphabet;
+            break;
+        case 'e':
+            document.getElementById("audiosrc").src = "sounds/e.mp3";
+            audio.load(); //call this to just preload the audio without playing.
+            audio.play(); //call this to play the song right away.
+            document.getElementById("big-image").src = "img-alphabet/" + small_alphabet;
+            break;
+        case 'r':
+            document.getElementById("audiosrc").src = "sounds/r.mp3";
+            audio.load(); //call this to just preload the audio without playing.
+            audio.play(); //call this to play the song right away.
+            document.getElementById("big-image").src = "img-alphabet/" + small_alphabet;
+            break;
+        case 't':
+            document.getElementById("audiosrc").src = "sounds/t.mp3";
+            audio.load(); //call this to just preload the audio without playing.
+            audio.play(); //call this to play the song right away.
+            document.getElementById("big-image").src = "img-alphabet/" + small_alphabet;
+            break;
+        case 'y':
+            document.getElementById("audiosrc").src = "sounds/y.mp3";
+            audio.load(); //call this to just preload the audio without playing.
+            audio.play(); //call this to play the song right away.
+            document.getElementById("big-image").src = "img-alphabet/" + small_alphabet;
+            break;
+        case 'u':
+            document.getElementById("audiosrc").src = "sounds/u.mp3";
+            audio.load(); //call this to just preload the audio without playing.
+            audio.play(); //call this to play the song right away.
+            document.getElementById("big-image").src = "img-alphabet/" + small_alphabet;
+            break;
+        case 'i':
+            document.getElementById("audiosrc").src = "sounds/i.mp3";
+            audio.load(); //call this to just preload the audio without playing.
+            audio.play(); //call this to play the song right away.
+            document.getElementById("big-image").src = "img-alphabet/" + small_alphabet;
+            break;
+        case 'o':
+            document.getElementById("audiosrc").src = "sounds/o.mp3";
+            audio.load(); //call this to just preload the audio without playing.
+            audio.play(); //call this to play the song right away.
+            document.getElementById("big-image").src = "img-alphabet/" + small_alphabet;
+            break;
+        case 'p':
+            document.getElementById("audiosrc").src = "sounds/p.mp3";
+            audio.load(); //call this to just preload the audio without playing.
+            audio.play(); //call this to play the song right away.
+            document.getElementById("big-image").src = "img-alphabet/" + small_alphabet;
+            break;
+        // ROw 2nd
+        case 'a':
+            document.getElementById("audiosrc").src = "sounds/a.mp3";
+            audio.load(); //call this to just preload the audio without playing.
+            audio.play(); //call this to play the song right away.
+            document.getElementById("big-image").src = "img-alphabet/" + small_alphabet;
+            break;
+        case 's':
+            document.getElementById("audiosrc").src = "sounds/s.mp3";
+            audio.load(); //call this to just preload the audio without playing.
+            audio.play(); //call this to play the song right away.
+            document.getElementById("big-image").src = "img-alphabet/" + small_alphabet;
+            break;
+        case 'd':
+            document.getElementById("audiosrc").src = "sounds/d.mp3";
+            audio.load(); //call this to just preload the audio without playing.
+            audio.play(); //call this to play the song right away.
+            document.getElementById("big-image").src = "img-alphabet/" + small_alphabet;
+            break;
+        case 'f':
+            document.getElementById("audiosrc").src = "sounds/f.mp3";
+            audio.load(); //call this to just preload the audio without playing.
+            audio.play(); //call this to play the song right away.
+            document.getElementById("big-image").src = "img-alphabet/" + small_alphabet;
+            break;
+        case 'g':
+            document.getElementById("audiosrc").src = "sounds/g.mp3";
+            audio.load(); //call this to just preload the audio without playing.
+            audio.play(); //call this to play the song right away.
+            document.getElementById("big-image").src = "img-alphabet/" + small_alphabet;
+            break;
+        case 'h':
+            document.getElementById("audiosrc").src = "sounds/h.mp3";
+            audio.load(); //call this to just preload the audio without playing.
+            audio.play(); //call this to play the song right away.
+            document.getElementById("big-image").src = "img-alphabet/" + small_alphabet;
+            break;
+        case 'j':
+            document.getElementById("audiosrc").src = "sounds/j.mp3";
+            audio.load(); //call this to just preload the audio without playing.
+            audio.play(); //call this to play the song right away.
+            document.getElementById("big-image").src = "img-alphabet/" + small_alphabet;
+            break;
+        case 'k':
+            document.getElementById("audiosrc").src = "sounds/k.mp3";
+            audio.load(); //call this to just preload the audio without playing.
+            audio.play(); //call this to play the song right away.
+            document.getElementById("big-image").src = "img-alphabet/" + small_alphabet;
+            break;
+        case 'l':
+            document.getElementById("audiosrc").src = "sounds/l.mp3";
+            audio.load(); //call this to just preload the audio without playing.
+            audio.play(); //call this to play the song right away.
+            document.getElementById("big-image").src = "img-alphabet/" + small_alphabet;
+            break;
+
+        // Row 3rd
+        case 'z':
+            document.getElementById("audiosrc").src = "sounds/z.mp3";
+            audio.load(); //call this to just preload the audio without playing.
+            audio.play(); //call this to play the song right away.
+            document.getElementById("big-image").src = "img-alphabet/" + small_alphabet;
+            break;
+        case 'x':
+            document.getElementById("audiosrc").src = "sounds/x.mp3";
+            audio.load(); //call this to just preload the audio without playing.
+            audio.play(); //call this to play the song right away.
+            document.getElementById("big-image").src = "img-alphabet/" + small_alphabet;
+            break;
+        case 'c':
+            document.getElementById("audiosrc").src = "sounds/c.mp3";
+            audio.load(); //call this to just preload the audio without playing.
+            audio.play(); //call this to play the song right away.
+            document.getElementById("big-image").src = "img-alphabet/" + small_alphabet;
+            break;
+        case 'v':
+            document.getElementById("audiosrc").src = "sounds/v.mp3";
+            audio.load(); //call this to just preload the audio without playing.
+            audio.play(); //call this to play the song right away.
+            document.getElementById("big-image").src = "img-alphabet/" + small_alphabet;
+            break;
+        case 'b':
+            document.getElementById("audiosrc").src = "sounds/b.mp3";
+            audio.load(); //call this to just preload the audio without playing.
+            audio.play(); //call this to play the song right away.
+            document.getElementById("big-image").src = "img-alphabet/" + small_alphabet;
+            break;
+        case 'n':
+            document.getElementById("audiosrc").src = "sounds/n.mp3";
+            audio.load(); //call this to just preload the audio without playing.
+            audio.play(); //call this to play the song right away.
+            document.getElementById("big-image").src = "img-alphabet/" + small_alphabet;
+            break;
+        case 'm':
+            document.getElementById("audiosrc").src = "sounds/m.mp3";
+            audio.load(); //call this to just preload the audio without playing.
+            audio.play(); //call this to play the song right away.
+            document.getElementById("big-image").src = "img-alphabet/" + small_alphabet;
+            break;
+
+    }
+}
+
+var key = document.querySelectorAll('key').length;
+
+for (let i = 0; i < key; i++) {
+    document.querySelectorAll('key')[i].addEventListener('click', function () {
+        var buttonHTML = this.innerHTML;
+        sounds(buttonHTML);
+    })
+}
+// Image change on click.
+// let key = document.getElementsByClassName('key');
+// console.log(key);---worked.
+
+// where i want to display.
+// var big_image = document.getElementById('big-image');
+
+// key to display.
+// var key = document.getElementsByClassName('key');
+
+// from where i want to display img.
+// created a pics array.
+// var pics = {
+//     "img-alphabet:": ["q.jpg", "w.jpg", "e.jpg", "r.jpg", "t.jpg", "y.jpg"]
+// };
+
+// var p;
+
+// var key = document.getElementsByClassName('key');
+
+// !For...Loop...This is the or loop syntax.
+// for (let p = 0; p < key.length; p++) {
+//     key[p].addEventListener('click', function () {
+//         alert("Hello")
+//         big_image.src = pics.src;
+//         // big-image.src = small_image.src.replace();
+//         // document.getElementsByid(big-image).innerHTML.style.display.block;
+//         document.getElementsByClassName('')
+//     })
+
+// }
+
+
+
